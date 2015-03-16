2845e02c4fffb0a5b23a7b02fc44ecbd021dfcde
DavidLambertCyber <DavidLambertCyber@gmail.com>
2015-03-15 21:41:20 -0400

    Decode Pico2013 Bitwise challenge
    Tested on python2.7 in Ubuntu 14

---
+++ solve_pico_bit.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+#tested on python2.7
+
+#encryption array
+#user_arr.append( ( ( (thing << 5) | (thing >> 3) ) ^ 111) & 255 )
+
+verify_arr = [193, 35, 9, 33, 1, 9, 3, 33, 9, 225]
+user_arr = []
+done_arr = []
+
+
+for thing in verify_arr:
+  axor = (thing ^ 111)
+  aor = (axor >> 5) | (axor << 3)
+  user_arr.append(aor & 255)
+
+
+print user_arr
+
+for bla in user_arr:
+  done_arr.append(chr(bla))
+
+print done_arr
+
+
